9d3272c836cfe8a532dcc4bea7766d57a7ca0e9c
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-07-30 14:31:08 +0900

    [add]エネミーの動きを追加

---
+++ Assets/Shiomi/WalkEnemy.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class WalkEnemy : MonoBehaviour
+{
+    //追跡するプレイヤーを追加
+    [SerializeField] GameObject _player;
+    //追跡するスピード
+    [SerializeField,Range(0,100)] float _enemySpeed = 1;
+    void Strat()
+    {
+        _player = GameObject.FindWithTag("Player");
+    }
+    private void FixedUpdate()
+    {
+        //方向を決める
+        Vector3 dir = (_player.transform.position - this.transform.position).normalized;
+        //移動方向とスピードを決定する
+        transform.position += dir * _enemySpeed * Time.deltaTime;
+        //正面を向く
+        if(dir.magnitude > 0)
+        {
+            transform.rotation = Quaternion.LookRotation(dir);
+        }
+    }
+}
