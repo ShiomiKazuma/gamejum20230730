99816b8ba83c1a2b87e09cd0f79d8358dcff4b26
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-07-30 15:32:04 +0900

    [add]エネミージェネレータの作成

---
+++ Assets/Shiomi/EnemyGenerator.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    //敵のプレパブ
+    [SerializeField] GameObject _enemyPrefab;
+    //敵生成時間の設定
+    [SerializeField, Range(0,10)] float interval = 5.0f;
+    //タイマー
+    float _timer;
+    private void Start()
+    {
+        _timer = interval;
+    }
+    void FixedUpdate() 
+    {
+        _timer += Time.deltaTime;
+        if(_timer >= interval)
+        {
+            GameObject enemy = GameObject.Instantiate(_enemyPrefab);
+            _timer = 0;
+        }
+    }
+}
